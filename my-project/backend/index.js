7dd30aa2dd1d5023107518d611dc55c6129fec0d
mdMERAJ77 <mmd200975@gmail.com>
2026-05-03 09:08:32 +0530

    update

--- my-project/backend/index.js
+++ my-project/backend/index.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+app.get("/", (req, res) => res.send("Backend is running!"));
 
 // ✅ CORS properly configure karo
 app.use(cors({
